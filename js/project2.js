2db17704fbf5e4294b1984c991041c491660e91f
Omar Mayallo <62558914+omar-mayallo@users.noreply.github.com>
2020-05-12 07:18:32 +0200

    simple portofolio

---
+++ js/project2.js
@@ -0,0 +1,21 @@
+var home = document.getElementById("home"),
+    
+    resume = document.getElementById("resume"),
+    
+    partOne = document.getElementByClassName("partone"),
+    
+    partTwo = document.getElementByClassName("parttwo"),
+    
+    partThree = document.getElementByClassName("partthree"),
+    
+    partFour = document.getElementByClassName("partfour");
+    
+
+partTwo.onclick = function () {
+    
+    "use strict";
+    
+    home.style.display = "none";
+};
+
+
